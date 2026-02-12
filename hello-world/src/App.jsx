c66579705f38c5b18acc34eb4ae1880fdbdbe910
Miko-29 <muskan2912002@gmail.com>
2026-02-12 22:54:14 +0530

    rules for jsx

--- hello-world/src/App.jsx
+++ hello-world/src/App.jsx
@@ -2,10 +2,18 @@ import './App.css'
 import Welcome from './Welcome'
 import Button from './button';
 import {Hello, HelloWithoutJSX} from './Hello';
+import { UserProfile } from "./UserProfile";
+import { ContactForm } from './ContactForm';
+import { StyledForm } from './StyledForm';
+import { CandidateProfile } from './CandidateProfile';
 
 function App() {
   return (
    <div>
+    <CandidateProfile/>
+    <StyledForm />
+    <ContactForm/>
+    <UserProfile />
     <Hello />
     <HelloWithoutJSX/>
     <Welcome/>    
