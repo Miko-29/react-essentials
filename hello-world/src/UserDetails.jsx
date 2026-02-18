5159b26ef8c791972acd60273794ab33e70a3c00
Miko-29 <muskan2912002@gmail.com>
2026-02-18 22:49:46 +0530

    ternary conditional rendering

--- hello-world/src/UserDetails.jsx
+++ hello-world/src/UserDetails.jsx
@@ -1,26 +1,49 @@
-export const UserDetails = ({ name, isOnline, hideOffline }) => {
+export const UserDetails = ({ name, isOnline, hideOffline, isPremium, isNewUser}) => {
     if(hideOffline && !isOnline){
         return null;
     }
    
-    if(isOnline){
-    return(
-        <div>
-            <h3>{name}</h3>
-            <span>🟢 Online</span>
-            <p>Available for chat</p>
-            <button>Send Message</button>
-        </div>
-    );
-   }
+    // ternary operators
 
-   return (
+    return (
         <div>
-            <h3>{name}</h3>
-            <span>Offline</span>
-            <p>Not available</p>
-            <small>Check back later</small>
+            <h3>
+                {name}
+                
+            </h3>
+            <span>{isOnline ? "🟢 Online" : "Offline"}</span>
+            <p>{isOnline ? "Available for chat" : "Not available"}</p>
+            {
+                isOnline ? (
+                    <button>Send Message</button>
+                ) : (
+                    <small>Check back later</small>
+                )
+            }
         </div>
-   )
+    )
+
+
+    // If conditions
+
+//     if(isOnline){
+//     return(
+//         <div>
+//             <h3>{name}</h3>
+//             <span>🟢 Online</span>
+//             <p>Available for chat</p>
+//             <button>Send Message</button>
+//         </div>
+//     );
+//    }
+
+//    return (
+//         <div>
+//             <h3>{name}</h3>
+//             <span>Offline</span>
+//             <p>Not available</p>
+//             <small>Check back later</small>
+//         </div>
+//    )
 
 }
