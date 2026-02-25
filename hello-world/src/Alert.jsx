d4018c05217746c916e79ef3378d72bbcdf042b1
Miko-29 <muskan2912002@gmail.com>
2026-02-25 23:31:51 +0530

    css modules

--- hello-world/src/Alert.jsx
+++ hello-world/src/Alert.jsx
@@ -1,5 +1,9 @@
-import "./Alert.css";
+import styles from "./Alert.module.css";
 
 export const Alert = ({children, type = "success"}) =>{
-    return <div className={`alert ${type}`}>{children}</div>
+    // return <div className={`alert ${type}`}>{children}</div>
+
+    return <div className={`${styles.alert} ${styles[type]}`}>{children}</div> 
+    // we use template literals to combine the base alert class and type specific class
+    // we use bracket[] for type because it is a dynamic value
 }
