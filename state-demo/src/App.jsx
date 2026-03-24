f2ab25f26d34581d9251c2788ecd208177e4556b
Miko-29 <muskan2912002@gmail.com>
2026-03-24 22:29:13 +0530

    useState using useReducer

--- state-demo/src/App.jsx
+++ state-demo/src/App.jsx
@@ -14,6 +14,7 @@ import { ShoppingCart } from "./ShoppingCart";
 import { CounterWithReducer } from "./CounterWithReducer";
 import { ShoppingCartWithReducer } from "./ShoppingCartWithReducer";
 import { CounterWithInit } from "./CounterWithInit";
+import { CustomCounter } from "./CustomCounter";
 
 function App() {
   return (
@@ -32,7 +33,8 @@ function App() {
       {/* <ShoppingCart/> */}
       {/* <CounterWithReducer/> */}
       {/* <ShoppingCartWithReducer /> */}
-      <CounterWithInit />
+      {/* <CounterWithInit /> */}
+      <CustomCounter />
     </>
   );
 }
