fc9fc4cc13207be0f89072776629ca9b4e472181
Miko-29 <muskan2912002@gmail.com>
2026-03-26 21:18:17 +0530

    context-demo

---
+++ context-demo/src/App.jsx
@@ -0,0 +1,19 @@
+import "./App.css";
+import { Header } from "./header";
+
+function App(){
+  const user = {
+    name : "Bruce Wayne",
+    role : "admin",
+    theme : "dark"
+  }
+
+  return (
+    <div>
+      <h1>Dashboard</h1>
+      <Header user={user} />
+    </div>
+  )
+}
+
+export default App;
