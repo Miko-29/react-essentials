fff2b8ff66b93b78ec1b6999534c4a62048ec62a
Miko-29 <muskan2912002@gmail.com>
2026-03-27 23:37:14 +0530

    refs-demo

---
+++ refs-demo/src/FocusInput.jsx
@@ -0,0 +1,14 @@
+import { useRef } from "react";
+
+export const FocusInput = () => {
+    const inputRef = useRef(null);
+
+    const handleInputFocus = () => {
+        inputRef.current.focus()
+    }
+
+    return <div>
+        <input ref={inputRef} type="text" placeholder="Enter your name" />
+        <button onClick={handleInputFocus}>Focus Input</button>
+    </div>
+}
